86baad0f282e39afcac3728d8ba52ad0fb41ef02
mclee <myoungcheol.lee@bluebirdcorp.com>
2024-04-05 19:05:13 +0900

    fix: check condition overhead

--- Prgs/Prgs12906.py
+++ Prgs/Prgs12906.py
@@ -1,10 +1,11 @@
 # 같은 숫자는 싫어
 # https://school.programmers.co.kr/learn/courses/30/lessons/12906
 
+# arr 크기는 자연수 (1 ~ 1백만 이하 크기)
 def solution(arr):
-    result = []
+    result = [arr[0]]
     for num in arr:
-        if not result or result[-1] != num: 
+        if result[-1] != num: 
             result.append(num)
     return result
 
